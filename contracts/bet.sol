c330462d3d8c331ef1943dbf6679effa4f7448d9
Remix Workspace <>
2022-11-07 11:09:53 -0800

    fix: errors for successful compile

--- contracts/bet.sol
+++ contracts/bet.sol
@@ -10,7 +10,6 @@ struct Bet {
     string assetNname;
     address adrs;
     uint amount;
-    uint betEndTime;
     Price priceBet;
 }
 
@@ -32,20 +31,21 @@ Bet[] public bets;
 Price[] public prices;
     
 address payable betContractOwner;
+uint public betEndTime;
 uint public totalMoneyOnBet = 0;
 
 mapping (address => uint) public numberOfBetsPerAddress;
 
 event BetEnded(bool wasPriceGreaterThan);
 
-constructor(string _assetName, uint _price, uint bettingTime) payable {
+constructor(string memory _assetName, uint  _price, uint bettingTime) payable {
     betContractOwner = payable(msg.sender); // setting the contract creator - this person cannot bet
     prices.push(Price(_assetName, _price, true, 0)); // the first index will be for bets that it will be greater than
     prices.push(Price(_assetName, _price, false, 0)); // the second index will be for bets that it will be less than 
     betEndTime = block.timestamp + bettingTime;
 }
 
-function createPrice(string memory _assetName, uint memory _price, bool memory _greaterThan) public {
+function createPrice(string memory _assetName, uint _price, bool _greaterThan) public {
     prices.push(Price(_assetName, _price, _greaterThan, 0));
 }
 
@@ -56,7 +56,7 @@ function getTotalBetAmount (uint _priceId) public view returns (uint) {
 
 function createBet (string memory _assetName, uint _priceId) external payable {       
     require (msg.sender != betContractOwner, "Sorry, the owner can't make a bet");
-    require (numBetsAddress[msg.sender] == 0, "Sorry, you have already placed a bet");
+    require (numberOfBetsPerAddress[msg.sender] == 0, "Sorry, you have already placed a bet");
     require (msg.value > 0.001 ether, "Error! You have to bet more than 0.001 ether");
     require((block.timestamp < betEndTime) , "Sorry! The betting window is over! ");
 
@@ -68,25 +68,25 @@ function createBet (string memory _assetName, uint _priceId) external payable {
     } 
     // 0 means this person thinks the price will be less than 
     if (_priceId == 1) {
-        pricess[1].totalBetAmount += msg.value;
+        prices[1].totalBetAmount += msg.value;
     }
 
     (bool sent, bytes memory data) = betContractOwner.call{value: msg.value}("");
     require(sent, "Failed to send Ether. Try again.");
 
-    totalBetMoney += msg.value;
+   totalMoneyOnBet += msg.value;
 
     emit NewBet(msg.sender, msg.value, prices[_priceId]);
 }
 
 // Distribute the funds to the winners. Only the owner of the contract can set the winner. Should connect to data oracle...
-function betWinDistribution(uint _priceId) public payable onlyOwner() {
+function betWinDistribution(uint _priceId) public payable  {
         
     uint div;
 
     if (_priceId == 0) {
         for (uint i = 0; i < bets.length; i++) {
-            if (keccak256(abi.encodePacked((bets[i].priceBet.name))) == keccak256(abi.encodePacked("greaterThanBetters"))) {
+            if (keccak256(abi.encodePacked((bets[i].priceBet.assetName))) == keccak256(abi.encodePacked("greaterThanBetters"))) {
                 address payable receiver = payable(bets[i].adrs);
                 div = (bets[i].amount * (10000 + (getTotalBetAmount(1) * 10000 / getTotalBetAmount(0)))) / 10000;
 
@@ -97,7 +97,7 @@ function betWinDistribution(uint _priceId) public payable onlyOwner() {
         }
     } else {
         for (uint i = 0; i < bets.length; i++) {
-            if (keccak256(abi.encodePacked((bets[i].teamBet.name))) == keccak256(abi.encodePacked("lessThanBetters"))) {
+            if (keccak256(abi.encodePacked((bets[i].priceBet.assetName))) == keccak256(abi.encodePacked("lessThanBetters"))) {
                 address payable receiver = payable(bets[i].adrs);
                 div = (bets[i].amount * (10000 + (getTotalBetAmount(0) * 10000 / getTotalBetAmount(1)))) / 10000;
 
@@ -107,12 +107,12 @@ function betWinDistribution(uint _priceId) public payable onlyOwner() {
         }
     }
 
-    totalBetMoney = 0;
+    totalMoneyOnBet = 0;
     prices[0].totalBetAmount = 0;
     prices[1].totalBetAmount = 0;
 
     for (uint i = 0; i < bets.length; i++) {
-        numBetsAddress[bets[i].adrs] = 0;
+        numberOfBetsPerAddress[bets[i].adrs] = 0;
     }
 
 }
